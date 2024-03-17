82e11046923a309a0c7efd264236b6a274d20727
MohammedMaisoon <mmohammedmaisoon@gmail.com>
2024-03-17 14:49:44 +0400

    Initial commit

---
+++ src/codeoftheday/pyramid1.java
@@ -0,0 +1,24 @@
+package codeoftheday;
+public class pyramid1 {
+
+    public static void main(String[] args) {
+        secondTriangle();
+    }
+    public static void secondTriangle() {
+            int noOfRows = 5;
+            int rowCount = noOfRows;
+            for (int i = 0; i < noOfRows; i++) {
+                for (int j = 1; j <= i * 2; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j <= rowCount; j++) {
+                    System.out.print(j + " ");
+                }
+                //Printing j where j value will be from rowCount-1 to 1
+                for (int j = rowCount - 1; j >= 1; j--) {
+                    System.out.print(j + " ");
+                }
+                System.out.println();
+                rowCount--;
+            }
+        }}
